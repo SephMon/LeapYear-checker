eac21e9b3a8e69adbb413ed7d7009e8cb5e019f1
SephMon <kevinosullivan25@gmail.com>
2019-01-01 20:37:59 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Boolean exit = true;
+            int year = 0;
+            while(exit){
+               
+                year = getInput(); 
+                exit = checkInput(year);
+                
+                if(checkInput(year)==false){
+                    Exit();
+                    break; 
+                }
+                 Console.WriteLine("Message: " +checkYear(year));
+            }
+        }
+        public static int getInput(){
+             Console.Write("Please enter a year: ");
+              int year = Convert.ToInt32(Console.ReadLine());
+              return year;
+        }
+        public static Boolean checkYear(int year){
+              if(year%400 == 0){
+                  return true;
+              }
+              else if(year %100==0){
+                return false;
+              }
+              else if(year%4==0){
+                  return true;
+              }
+              return false;
+        }
+        public static Boolean checkInput(int year){
+              if(year<=0){
+                return false;
+                }  
+              else{
+                return true;
+             } 
+        }
+        public static void Exit(){
+            Console.WriteLine("Exiting");
+        }
+        
+        
+       
+    }    
+}
+
+
